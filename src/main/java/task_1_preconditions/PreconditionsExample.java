610d2f19e2b50f4db5dfba7604c4971863ba9f3c
RafalJDev <jaszczynski.rafal@gmail.com>
2018-06-07 09:44:57 +0200

    Task 1

---
+++ src/main/java/task_1_preconditions/PreconditionsExample.java
@@ -0,0 +1,29 @@
+package task_1_preconditions;
+
+/**
+ * Created by Jaszczynski.Rafal on 17.05.2018.
+ */
+public class PreconditionsExample {
+
+  /*
+    todo Create static methods:
+
+    todo 1. checkArgument
+      that will throw IllegalArgumentException if parameter is false
+
+    todo 2. checkNotNull
+      that will throw NullPointerException if parameter is null
+
+    todo 3. checkElementIndex
+      that will throw IndexOutOfBoundsException if provided index,
+      is out of provided arrays length(exclusive)
+
+    todo 4. checkPositionIndexes,
+      that will throw IndexOutOfBoundsException, if provided start and end index,
+      is out of provided arrays length(exclusive)
+    */
+
+  public static void main(String[] args) {
+
+  }
+}
