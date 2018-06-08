261d577138def0bbbb4611fb2899f6f70f17f90c
RafalJDev <jaszczynski.rafal@gmail.com>
2018-06-08 09:39:06 +0200

    Task 2, ups

---
+++ src/main/java/task_2_primitives/PrimitivesExample.java
@@ -0,0 +1,43 @@
+package task_2_primitives;
+
+public class PrimitivesExample {
+
+  public static void main(String[] args) {
+
+    /*
+    todo Create statics methods
+
+    todo 1. max,
+      with given array of ints, return max value
+
+    todo 2. min,
+      with given array of ints, return min value
+
+    todo 3. concat,
+      with given n arrays, concatenate arrays all arrays to one array
+      and return this new array
+
+    todo 4. reverse
+      with given array reverse all its elements
+      and return that array
+    */
+
+  }
+
+  public static int max(int[] array) {
+    return 0;
+  }
+
+  public static int min(int[] array) {
+    return 0;
+  }
+
+  public static int[] concat(int[] array1, int[] array2) {
+    return null;
+  }
+
+  public static int[] reverse(int[] array) {
+    return null;
+  }
+
+}
