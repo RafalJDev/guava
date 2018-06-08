483e6fc2ea9a34fc05398e1ce036d6348fce2dd9
RafalJDev <jaszczynski.rafal@gmail.com>
2018-06-08 09:57:56 +0200

    Task 3

---
+++ src/main/java/strings/task_3/StringsExample.java
@@ -0,0 +1,38 @@
+package strings.task_3;
+
+/**
+ * Created by Jaszczynski.Rafal on 16.05.2018.
+ */
+public class StringsExample {
+
+  /*
+    todo create static methods
+
+    todo 1. isNullOrEmpty
+      Takes string and checks if it's null or empty
+      return result as boolean
+
+    todo 2. repeat
+      Takes string and count
+      and return this  string repeated x times
+
+      e.g. repeat("Jo je dupny ", 3)
+      "Jo je dupny Jo je dupny Jo je dupny "
+
+    todo 3. padEnd
+    takes string, minLength and padChar
+    and return string with appended padChar
+
+    Simply add x times padChar to the end of given string
+    x = string.length() - minLength
+
+    Ask me and I will explain
+
+    e.g.
+    String string = "Kaj leziesz, tam je Sosnowiec";
+    String padEnd = Strings.padEnd(string, string.length() + 10, '!');
+    padEnd -> Kaj leziesz, tam je Sosnowiec!!!!!!!!!!
+
+     */
+
+}
